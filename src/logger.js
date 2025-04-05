4d4d7841ff98329ebcfd1c75a6b4dd112b941642
aonstott <aonstott32@gmail.com>
2025-04-04 20:36:42 -0600

    bruh

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,1 @@
-function sendLogToGrafana(event) {
-  const body = JSON.stringify(event);
-  fetch(`${config.url}`, {
-    method: "post",
-    body: body,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${config.userId}:${config.apiKey}`,
-    },
-  }).then((res) => {
-    if (!res.ok) console.log("Failed to send log to Grafana");
-  });
-}
+
